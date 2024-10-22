ebc0d2dc78d30a9820ccdddef6822bef8a78b1bc
pulkit-mckinley <pulkit.guglani@mckinleyrice.co>
2024-10-22 16:00:34 +0530

    Chore: utilised expense class functions and removed from expense report

--- src/main/java/victor/training/cleancode/kata/expense/ExpenseReport.java
+++ src/main/java/victor/training/cleancode/kata/expense/ExpenseReport.java
@@ -1,46 +1,46 @@
 package victor.training.cleancode.kata.expense;
 
-import java.util.Date;
 import java.util.List;
 
 public class ExpenseReport {
+
     public void printReport(List<Expense> expenses) {
-        int total = 0;
+        int totalExpense = 0;
         int mealExpenses = 0;
-        boolean mealOver = false;
+        boolean isMealOver = false;
 
         System.out.println("Expenses Report");
 
         for (Expense expense : expenses) {
-            if (expense.type.equals("DINNER") || expense.type.equals("BREAKFAST")) {
+            if (expense.isBreakfastOrDinner()) {
                 mealExpenses += expense.amount;
             }
 
             String en = null;
             switch (expense.type) {
-            case "DINNER":
-                en = "Dinner";
-                break;
-            case "BREAKFAST":
-                en = "Breakfast";
-                break;
-            case "CAR_RENTAL":
-                en = "Car Rental";
-                break;
+                case "DINNER":
+                    en = "Dinner";
+                    break;
+                case "BREAKFAST":
+                    en = "Breakfast";
+                    break;
+                case "CAR_RENTAL":
+                    en = "Car Rental";
+                    break;
             }
 
             // meal over expenses
             String m = expense.type.equals("DINNER") && expense.amount > 5000 || expense.type.equals("BREAKFAST") && expense.amount > 1000 ? "X" : " ";
-            mealOver |= m.equals("X");
+            isMealOver |= m.equals("X");
 
             System.out.println(en + "\t" + expense.amount + "\t" + m);
 
-            total += expense.amount;
+            totalExpense += expense.amount;
         }
 
         System.out.println("Meal expenses: " + mealExpenses);
-        System.out.println("Total expenses: " + total);
-        if (mealOver) {
+        System.out.println("Total expenses: " + totalExpense);
+        if (isMealOver) {
             System.out.println("Meal expenses exceed limit");
         }
     }
