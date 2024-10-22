f39d2f98101aefc2ad7aed719b40f8890cd9ee47
pulkit-mckinley <pulkit.guglani@mckinleyrice.co>
2024-10-22 15:59:43 +0530

    Chore: created functions in expense class to get english name and to get breakfast or dinner bool

--- src/main/java/victor/training/cleancode/kata/expense/Expense.java
+++ src/main/java/victor/training/cleancode/kata/expense/Expense.java
@@ -1,10 +1,34 @@
 package victor.training.cleancode.kata.expense;
 
 public class Expense {
+
     String type;
     int amount;
+
     public Expense(String type, int amount) {
         this.type = type;
         this.amount = amount;
+    }
+
+    public boolean isBreakfastOrDinner() {
+        return this.type.equals("BREAKFAST") || this.type.equals("DINNER");
+    }
+
+    public String getEnglishName() {
+        String en = "";
+        switch (this.type) {
+            case "DINNER":
+                en = "Dinner";
+                break;
+            case "BREAKFAST":
+                en = "Breakfast";
+                break;
+            case "CAR_RENTAL":
+                en = "Car Rental";
+                break;
+        }
+
+        return en;
+
     }
 }
